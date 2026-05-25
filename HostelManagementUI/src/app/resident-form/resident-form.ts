4937d0cac031bd5ccdc3541d25fb36ecc0a25992
mousaghouri <2502130@students.au.edu.pk>
2026-05-26 03:58:04 +0500

    files of frontend by musa

---
+++ HostelManagementUI/src/app/resident-form/resident-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resident-form',
+  imports: [],
+  templateUrl: './resident-form.html',
+  styleUrl: './resident-form.css',
+})
+export class ResidentForm {}
---
+++ HostelManagementUI/src/app/resident-list/resident-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resident-list',
+  imports: [],
+  templateUrl: './resident-list.html',
+  styleUrl: './resident-list.css',
+})
+export class ResidentList {}
